5d8f09682b035c1666fa77b21b16e5585b438fef
nadiaGincoff <nadia.gincoff@gmail.com>
2020-10-06 14:57:44 -0300

    Updates

---
+++ precache-manifest.eed799daf53c7a0d56837e8038d96490.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5c95854491ac28b880fae33b4800e64",
+    "url": "/drink-finder/index.html"
+  },
+  {
+    "revision": "fb9fccd07b94d7110cc1",
+    "url": "/drink-finder/static/css/main.935cfb3f.chunk.css"
+  },
+  {
+    "revision": "def729d520b277604b0a",
+    "url": "/drink-finder/static/js/2.caf9ba89.chunk.js"
+  },
+  {
+    "revision": "58aabe32aae325c8d2caf2ff6956fc48",
+    "url": "/drink-finder/static/js/2.caf9ba89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb9fccd07b94d7110cc1",
+    "url": "/drink-finder/static/js/main.4eba51b3.chunk.js"
+  },
+  {
+    "revision": "871696f62568ccc3b4d1",
+    "url": "/drink-finder/static/js/runtime-main.b9c9402d.js"
+  }
+]);
